2f735a2298b47a2ca2c4c3eb1c0962edccb9e68c
Fernando <espana.garcia@netzwerkplan.de>
2022-09-14 10:03:58 +0200

    correct error calling

--- backend/database.js
+++ backend/database.js
@@ -4,4 +4,4 @@ const databaseURI = 'mongodb://localhost/blog-database'
 
 mongoose.connect(databaseURI)
 .then(db => console.log('Database conected'))
-.catch(e => console.error(err));
+.catch(e => console.error(e));
