0b11bfa420dc69b976d3200803a41c99c02b0694
Fernando <espana.garcia@netzwerkplan.de>
2022-11-03 19:13:14 +0100

    add test to create account

---
+++ frontend/src/app/app-account/app-create-account/app-create-account.harness.ts
@@ -0,0 +1,5 @@
+import { BaseHarnessFilters, ComponentHarness, HarnessPredicate } from '@angular/cdk/testing';
+
+export class AppCreateAccountHarness extends ComponentHarness {
+    static hostSelector = 'app-create-account';
+}
