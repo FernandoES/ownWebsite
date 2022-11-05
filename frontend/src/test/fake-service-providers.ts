653efd0dfcfea0e7cdeca2a02f92b4f748872ea5
Fernando <espana.garcia@netzwerkplan.de>
2022-11-05 11:21:57 +0100

    add test for login

--- frontend/src/test/fake-service-providers.ts
+++ frontend/src/test/fake-service-providers.ts
@@ -1,10 +1,25 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { off } from "process";
 import { of } from "rxjs";
+import { Petition } from "src/app/app-account/app-account.service";
 
 export class TestAppAccountService {
     sendLogin(mail: string, password: string) {
+        console.log('login Sent');
         return of(`${mail}${password}`);
+    }
+    fetchAccountPetitions() {
+        const petitions = [{
+            userName: 'userName1',
+            userMail: 'userMail1',
+            _id: 1
+        },
+        {
+            userName: 'userName2',
+            userMail: 'userMail2',
+            _id: 2
+        }
+    ] as Petition[];
+    return of(petitions);
     }
 }
 
