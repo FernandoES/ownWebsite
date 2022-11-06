e4c75ae0184fcaaad1a66c1e55265b13d10c095a
Fernando <espana.garcia@netzwerkplan.de>
2022-11-06 14:19:12 +0100

    correct side menu testing

--- frontend/src/app/app-user/app-side-menu/app-side-menu.spec.ts
+++ frontend/src/app/app-user/app-side-menu/app-side-menu.spec.ts
@@ -1,15 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { AppSideMenuComponent } from './app-side-menu.component';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { AppSideMenuRoutingModule } from './app-side-menu-routing.module';
-import {MatMenuItemHarness} from '@angular/material/menu/testing';
+import {MatMenuHarness } from '@angular/material/menu/testing';
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-
+import { FakeUserService, routes, TestLanguagePipe } from 'src/test/fake-service-providers';
+import { UserService } from '../app-user.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonHarness} from '@angular/material/button/testing';
 
 describe('AppSideMenuComponent: ', () => {
     let component: AppSideMenuComponent;
@@ -24,9 +28,14 @@ describe('AppSideMenuComponent: ', () => {
                 MatAutocompleteModule,
                 MatIconModule,
                 AppSideMenuRoutingModule,
-                FormsModule ],
-            declarations: [AppSideMenuComponent],
-            providers: []
+                FormsModule,
+                ReactiveFormsModule,
+                RouterTestingModule.withRoutes(routes),
+                NoopAnimationsModule ],
+            declarations: [AppSideMenuComponent, TestLanguagePipe],
+            providers: [
+                {provide: UserService, useClass: FakeUserService},
+            ]
         });
     }));
 
@@ -35,6 +44,8 @@ describe('AppSideMenuComponent: ', () => {
         component = fixture.componentInstance;
         loader = await TestbedHarnessEnvironment.loader(fixture);
         fixture.detectChanges();
+        fixture.nativeElement.querySelector('button').click();
+        fixture.detectChanges();
     });
 
     it('should be created', () => {
@@ -42,9 +53,12 @@ describe('AppSideMenuComponent: ', () => {
     });
 
     it('should load three options',async () => {
-        const matMenuItemHarness = await loader.getAllHarnesses(MatMenuItemHarness);
-        expect(matMenuItemHarness.length).toBe(3);
+        fixture.whenStable().then(async ()=> {
+            const buttonHarness = await loader.getHarness(MatButtonHarness);
+            await buttonHarness.click();
+            const matMenuHarness = await loader.getHarness(MatMenuHarness);
+            const items = await matMenuHarness.getItems();
+            expect(items.length).toBe(3);
+        });
     });
-    
-
 });
